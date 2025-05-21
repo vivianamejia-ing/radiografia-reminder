e4c0284e6dea46e65445ad4f1a841b226256ec98
vivianamejia-ing <cuno3235@gmail.com>
2025-05-21 22:30:32 +0000

    install

---
+++ backend/mailer.js
@@ -0,0 +1,24 @@
+// backend/mailer.js
+const nodemailer = require('nodemailer');
+require('dotenv').config(); // Para usar variables de entorno
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail', // Puedes cambiar esto si usas otro correo (hotmail, outlook, etc.)
+  auth: {
+    user: process.env.EMAIL_USER, // Se lee de tus variables de entorno
+    pass: process.env.EMAIL_PASS
+  }
+});
+
+function enviarCorreo(destinatario, asunto, mensaje) {
+  const mailOptions = {
+    from: process.env.EMAIL_USER,
+    to: destinatario,
+    subject: asunto,
+    text: mensaje
+  };
+
+  return transporter.sendMail(mailOptions); // Esto envía el correo
+}
+
+module.exports = enviarCorreo;
