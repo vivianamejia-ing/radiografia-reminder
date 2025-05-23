ab10f3787c95b684a67612371b27ce762a76b39a
vivianamejia-ing <cuno3235@gmail.com>
2025-05-23 22:26:42 +0000

    .

--- backend/mailer.js
+++ backend/mailer.js
@@ -1,12 +1,13 @@
 // backend/mailer.js
 const nodemailer = require('nodemailer');
+const { getMaxListeners } = require('nodemailer/lib/xoauth2');
 require('dotenv').config(); // Para usar variables de entorno
 
 const transporter = nodemailer.createTransport({
   service: 'gmail', // Puedes cambiar esto si usas otro correo (hotmail, outlook, etc.)
   auth: {
-    user: process.env.EMAIL_USER, // Se lee de tus variables de entorno
-    pass: process.env.EMAIL_PASS
+    user: process.env.EMAIL_USER=cuno3235@gmail.com, // Se lee de tus variables de entorno
+    pass: process.env.EMAIL_PASS=fbvpjnjleborlfug
   }
 });
 
